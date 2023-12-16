996e3a7c9db232f55698e33168dcb844a72792a6
wermdany <wermdany@163.com>
2023-12-16 20:33:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">comfy-design</h1>
+
+A JavaScript Poster Design Tools
---
+++ packages/global.d.ts
@@ -0,0 +1,6 @@
+declare const __DEV__: boolean
+declare const __VERSION__: string
+declare const __TEST__: boolean
+declare const __ESM_BUNDLER__: boolean
+declare const __ESM_BROWSER__: boolean
+declare const __GLOBAL_BUILD__: boolean
